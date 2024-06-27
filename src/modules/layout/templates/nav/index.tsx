29ffe9d62faa56b796484232b4431e1e21a32f8e
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-06-27 14:16:34 +0800

    Update index.tsx

--- src/modules/layout/templates/nav/index.tsx
+++ src/modules/layout/templates/nav/index.tsx
@@ -1,16 +1,133 @@
-import { Suspense } from "react"
-
+"use client"
+import { Fragment, useState, Suspense } from 'react'
+import Link from 'next/link'
+import { Dialog } from '@headlessui/react'
 import LocalizedClientLink from "@modules/common/components/localized-client-link"
 import CartButton from "@modules/layout/components/cart-button"
 import SideMenu from "@modules/layout/components/side-menu"
 
-export default async function Nav() {
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default async function Head() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
   const regions = await listRegions().then((regions) => regions)
 
   return (
-    <div>
-               <SideMenu regions={regions} />
+    <header className="sticky top-0 inset-x-0 z-50 group bg-white dark:bg-gray-900 border-b border-gray-200 dark:border-gray-600">
+      <nav className="mx-auto flex max-w-7xl items-center justify-between p-3 lg:px-8 h-16" aria-label="Global">
+        <div className="flex lg:flex-1">
+          <div className="h-full">
             
-    </div>
+          </div>
+        </div>
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            onClick={() => setMobileMenuOpen(true)}
+          >
+            <span className="sr-only">打開菜單</span>
+            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 50 50">
+<path d="M 0 9 L 0 11 L 50 11 L 50 9 Z M 0 24 L 0 26 L 50 26 L 50 24 Z M 0 39 L 0 41 L 50 41 L 50 39 Z"></path>
+</svg>
+          </button>
+        </div>
+        <div className="flex items-center h-full">
+          <LocalizedClientLink
+            href="/"
+            className="txt-compact-xlarge-plus hover:text-ui-fg-base uppercase"
+            data-testid="nav-store-link"
+          >
+            <img className="h-8 w-auto" src="https://imgur.com/m3GZIgA.jpeg" alt="雙龍體育" />
+          </LocalizedClientLink>
+        </div>
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end items-center gap-x-6">
+          {process.env.FEATURE_SEARCH_ENABLED && (
+            <LocalizedClientLink
+              className="hover:text-ui-fg-base"
+              href="/search"
+              scroll={false}
+              data-testid="nav-search-link"
+            >
+              搜尋
+            </LocalizedClientLink>
+          )}
+          <LocalizedClientLink
+            className="hover:text-ui-fg-base"
+            href="/account"
+            data-testid="nav-account-link"
+          >
+            帳號
+          </LocalizedClientLink>
+          <Suspense
+            fallback={
+              <LocalizedClientLink
+                className="hover:text-ui-fg-base flex gap-2"
+                href="/cart"
+                data-testid="nav-cart-link"
+              >
+                購物車 (0)
+              </LocalizedClientLink>
+            }
+          >
+            <CartButton />
+          </Suspense>
+          
+        </div>
+      </nav>
+      <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+        <div className="fixed inset-0 z-10" />
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between">
+            <LocalizedClientLink href="/" className="-m-1.5 p-1.5">
+              <span className="sr-only">雙龍體育</span>
+              <img
+                className="h-8 w-auto"
+                src="https://imgur.com/m3GZIgA.jpeg"
+                alt=""
+              />
+            </LocalizedClientLink>
+            <button
+              type="button"
+              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              onClick={() => setMobileMenuOpen(false)}
+            >
+              <span className="sr-only">Close menu</span>
+              <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 50 50">
+<path d="M 0 9 L 0 11 L 50 11 L 50 9 Z M 0 24 L 0 26 L 50 26 L 50 24 Z M 0 39 L 0 41 L 50 41 L 50 39 Z"></path>
+</svg>
+            </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6">
+                {process.env.FEATURE_SEARCH_ENABLED && (
+                  <LocalizedClientLink
+                    href="/search"
+                    onClick={() => setMobileMenuOpen(false)}
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    scroll={false}
+                    data-testid="nav-search-link"
+                  >
+                    搜尋
+                  </LocalizedClientLink>
+                )}
+                <LocalizedClientLink
+                  href="/account"
+                  onClick={() => setMobileMenuOpen(false)}
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  data-testid="nav-account-link"
+                >
+                  帳號
+                </LocalizedClientLink>
+              </div>
+             
+            </div>
+          </div>
+        </Dialog.Panel>
+      </Dialog>
+    </header>
   )
 }

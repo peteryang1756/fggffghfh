df396c8c2ffa560aae85618186afa369fad9afbb
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-06-27 23:05:57 +0800

    Update index.tsx

--- src/modules/account/components/account-nav/index.tsx
+++ src/modules/account/components/account-nav/index.tsx
@@ -25,145 +25,145 @@ const AccountNav = ({
   }
 
   return (
-    <div>
-      <div className="small:hidden" data-testid="mobile-account-nav">
-        {route !== `/${countryCode}/account` ? (
-          <LocalizedClientLink
-            href="/account"
-            className="flex items-center gap-x-2 text-small-regular py-2"
-            data-testid="account-main-link"
-          >
-            <>
-              <ChevronDown className="transform rotate-90" />
-              <span>Account</span>
-            </>
-          </LocalizedClientLink>
-        ) : (
-          <>
-            <div className="text-xl-semi mb-4 px-8">
-              Hello {customer?.first_name}
-            </div>
-            <div className="text-base-regular">
-              <ul>
-                <li>
-                  <LocalizedClientLink
-                    href="/account/profile"
-                    className="flex items-center justify-between py-4 border-b border-gray-200 px-8"
-                    data-testid="profile-link"
-                  >
-                    <>
-                      <div className="flex items-center gap-x-2">
-                        <User size={20} />
-                        <span>Profile</span>
-                      </div>
-                      <ChevronDown className="transform -rotate-90" />
-                    </>
-                  </LocalizedClientLink>
-                </li>
-                <li>
-                  <LocalizedClientLink
-                    href="/account/addresses"
-                    className="flex items-center justify-between py-4 border-b border-gray-200 px-8"
-                    data-testid="addresses-link"
-                  >
-                    <>
-                      <div className="flex items-center gap-x-2">
-                        <MapPin size={20} />
-                        <span>Addresses</span>
-                      </div>
-                      <ChevronDown className="transform -rotate-90" />
-                    </>
-                  </LocalizedClientLink>
-                </li>
-                <li>
-                  <LocalizedClientLink
-                    href="/account/orders"
-                    className="flex items-center justify-between py-4 border-b border-gray-200 px-8"
-                    data-testid="orders-link"
-                  >
-                    <div className="flex items-center gap-x-2">
-                      <Package size={20} />
-                      <span>Orders</span>
-                    </div>
-                    <ChevronDown className="transform -rotate-90" />
-                  </LocalizedClientLink>
-                </li>
-                <li>
-                  <button
-                    type="button"
-                    className="flex items-center justify-between py-4 border-b border-gray-200 px-8 w-full"
-                    onClick={handleLogout}
-                    data-testid="logout-button"
-                  >
-                    <div className="flex items-center gap-x-2">
-                      <ArrowRightOnRectangle />
-                      <span>Log out</span>
-                    </div>
-                    <ChevronDown className="transform -rotate-90" />
-                  </button>
-                </li>
-              </ul>
-            </div>
-          </>
-        )}
-      </div>
-      <div className="hidden small:block" data-testid="account-nav">
-        <div>
-          <div className="pb-4">
-            <h3 className="text-base-semi">Account</h3>
-          </div>
-          <div className="text-base-regular">
-            <ul className="flex mb-0 justify-start items-start flex-col gap-y-4">
-              <li>
-                <AccountNavLink
-                  href="/account"
-                  route={route!}
-                  data-testid="overview-link"
-                >
-                  Overview
-                </AccountNavLink>
-              </li>
-              <li>
-                <AccountNavLink
-                  href="/account/profile"
-                  route={route!}
-                  data-testid="profile-link"
-                >
-                  Profile
-                </AccountNavLink>
-              </li>
-              <li>
-                <AccountNavLink
-                  href="/account/addresses"
-                  route={route!}
-                  data-testid="addresses-link"
-                >
-                  Addresses
-                </AccountNavLink>
-              </li>
-              <li>
-                <AccountNavLink
-                  href="/account/orders"
-                  route={route!}
-                  data-testid="orders-link"
-                >
-                  Orders
-                </AccountNavLink>
-              </li>
-              <li className="text-grey-700">
-                <button
-                  type="button"
-                  onClick={handleLogout}
-                  data-testid="logout-button"
-                >
-                  Log out
-                </button>
-              </li>
-            </ul>
-          </div>
+<div>
+  <div className="small:hidden" data-testid="mobile-account-nav">
+    {route !== `/${countryCode}/account` ? (
+      <LocalizedClientLink
+        href="/account"
+        className="flex items-center gap-x-2 text-small-regular py-2"
+        data-testid="account-main-link"
+      >
+        <>
+          <ChevronDown className="transform rotate-90" />
+          <span>帳戶</span>
+        </>
+      </LocalizedClientLink>
+    ) : (
+      <>
+        <div className="text-xl-semi mb-4 px-8">
+          你好 {customer?.first_name}
         </div>
+        <div className="text-base-regular">
+          <ul>
+            <li>
+              <LocalizedClientLink
+                href="/account/profile"
+                className="flex items-center justify-between py-4 border-b border-gray-200 px-8"
+                data-testid="profile-link"
+              >
+                <>
+                  <div className="flex items-center gap-x-2">
+                    <User size={20} />
+                    <span>個人資料</span>
+                  </div>
+                  <ChevronDown className="transform -rotate-90" />
+                </>
+              </LocalizedClientLink>
+            </li>
+            <li>
+              <LocalizedClientLink
+                href="/account/addresses"
+                className="flex items-center justify-between py-4 border-b border-gray-200 px-8"
+                data-testid="addresses-link"
+              >
+                <>
+                  <div className="flex items-center gap-x-2">
+                    <MapPin size={20} />
+                    <span>地址</span>
+                  </div>
+                  <ChevronDown className="transform -rotate-90" />
+                </>
+              </LocalizedClientLink>
+            </li>
+            <li>
+              <LocalizedClientLink
+                href="/account/orders"
+                className="flex items-center justify-between py-4 border-b border-gray-200 px-8"
+                data-testid="orders-link"
+              >
+                <div className="flex items-center gap-x-2">
+                  <Package size={20} />
+                  <span>訂單</span>
+                </div>
+                <ChevronDown className="transform -rotate-90" />
+              </LocalizedClientLink>
+            </li>
+            <li>
+              <button
+                type="button"
+                className="flex items-center justify-between py-4 border-b border-gray-200 px-8 w-full"
+                onClick={handleLogout}
+                data-testid="logout-button"
+              >
+                <div className="flex items-center gap-x-2">
+                  <ArrowRightOnRectangle />
+                  <span>登出</span>
+                </div>
+                <ChevronDown className="transform -rotate-90" />
+              </button>
+            </li>
+          </ul>
+        </div>
+      </>
+    )}
+  </div>
+  <div className="hidden small:block" data-testid="account-nav">
+    <div>
+      <div className="pb-4">
+        <h3 className="text-base-semi">帳戶</h3>
+      </div>
+      <div className="text-base-regular">
+        <ul className="flex mb-0 justify-start items-start flex-col gap-y-4">
+          <li>
+            <AccountNavLink
+              href="/account"
+              route={route!}
+              data-testid="overview-link"
+            >
+              概覽
+            </AccountNavLink>
+          </li>
+          <li>
+            <AccountNavLink
+              href="/account/profile"
+              route={route!}
+              data-testid="profile-link"
+            >
+              個人資料
+            </AccountNavLink>
+          </li>
+          <li>
+            <AccountNavLink
+              href="/account/addresses"
+              route={route!}
+              data-testid="addresses-link"
+            >
+              地址
+            </AccountNavLink>
+          </li>
+          <li>
+            <AccountNavLink
+              href="/account/orders"
+              route={route!}
+              data-testid="orders-link"
+            >
+              訂單
+            </AccountNavLink>
+          </li>
+          <li className="text-grey-700">
+            <button
+              type="button"
+              onClick={handleLogout}
+              data-testid="logout-button"
+            >
+              登出
+            </button>
+          </li>
+        </ul>
       </div>
     </div>
+  </div>
+</div>
   )
 }
 

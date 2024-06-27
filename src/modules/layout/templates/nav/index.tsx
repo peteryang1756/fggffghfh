4c9241f189bf90f561a6cb7d7af85af056441a9a
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-06-27 14:39:35 +0800

    Update index.tsx

--- src/modules/layout/templates/nav/index.tsx
+++ src/modules/layout/templates/nav/index.tsx
@@ -6,7 +6,6 @@ import { Navbar } from "flowbite-react";
 import { listRegions } from "@lib/data";
 import LocalizedClientLink from "@modules/common/components/localized-client-link";
 import CartButton from "@modules/layout/components/cart-button";
-import SideMenu from "@modules/layout/components/side-menu";
 
 export default async function Nav() {
   const regions = await listRegions().then((regions) => regions);
@@ -19,9 +18,7 @@ export default async function Nav() {
       </Navbar.Brand>
       <Navbar.Toggle />
       <Navbar.Collapse>
-        <div className="flex-1">
-          <SideMenu regions={regions} />
-        </div>
+        
         <Navbar.Link as={Link} href="/" active>
           Home
         </Navbar.Link>

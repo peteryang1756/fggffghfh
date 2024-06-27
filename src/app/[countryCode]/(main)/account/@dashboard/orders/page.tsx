f31c5820c9f1c3a7fd47b105052a3dc8699b5296
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-06-27 23:37:29 +0800

    Update page.tsx

--- src/app/[countryCode]/(main)/account/@dashboard/orders/page.tsx
+++ src/app/[countryCode]/(main)/account/@dashboard/orders/page.tsx
@@ -5,8 +5,8 @@ import { listCustomerOrders } from "@lib/data"
 import { notFound } from "next/navigation"
 
 export const metadata: Metadata = {
-  title: "Orders",
-  description: "Overview of your previous orders.",
+  title: "訂單",
+  description: "您之前訂單的概覽。",
 }
 
 export default async function Orders() {
@@ -19,10 +19,9 @@ export default async function Orders() {
   return (
     <div className="w-full" data-testid="orders-page-wrapper">
       <div className="mb-8 flex flex-col gap-y-4">
-        <h1 className="text-2xl-semi">Orders</h1>
+        <h1 className="text-2xl-semi">訂單</h1>
         <p className="text-base-regular">
-          View your previous orders and their status. You can also create
-          returns or exchanges for your orders if needed.
+          查看您之前的訂單及其狀態。如有需要，您還可以為您的訂單創建退貨或換貨。
         </p>
       </div>
       <div>
